1d5336b7c7cf0630c8d7d5896436f04c6b52d76d
Nikita <ncorpor@gmail.com>
2021-02-27 02:43:14 +0300

    - add build conf

---
+++ webpack.config.prod.js
@@ -0,0 +1,56 @@
+const path = require( 'path' );
+const HtmlWebpackPlugin = require( 'html-webpack-plugin' ); // Simplifies creation of HTML files to serve your webpack bundles
+
+module.exports = {
+    entry: [
+        './src/index.js'
+    ],
+
+    output: {
+        path: path.join( __dirname, './dist' ), // Output directory
+        filename: 'js/[name].[contenthash:6].js',
+        chunkFilename: 'js/[name].[contenthash:6].js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: [
+                    path.resolve( __dirname, 'src' )
+                ],
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: "style-loader",
+                    },
+                    {
+                        loader: "css-loader",
+                    },
+                    {
+                        loader: "less-loader",
+                        options: {
+                            lessOptions: {
+                                strictMath: true,
+                            },
+                        },
+                    },
+                ],
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({ // Simplifies creation of HTML files to serve your webpack bundles
+            template: './src/index.html',
+            inject: true,
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true
+            }
+        })
+    ]
+};
